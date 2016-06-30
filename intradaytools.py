b9e483941a0ee3b8c83192a2fa26c69e1aca1801
chriskoh <chris.koh90@gmail.com>
2016-06-30 15:32:44 -0700

    created file data merging algorithm, created tools.py

---
+++ intradaytools.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import datetime
+import os
+import requests
+from bs4 import BeautifulSoup, NavigableString
+
+def scrape(website):
+        set_url = website
+        cal_resp = requests.get(set_url)
+        cal_data = cal_resp.text
+        data = BeautifulSoup(cal_data, 'lxml')
+
+        return str(data)
+
+
+def temp(ticker, market):
+
+        # scrape data and write to a temp file
+        data = scrape('https://www.google.com/finance/getprices?q=' + str(ticker.upper()) + '&x=' + str(market.upper()) + '&i=60&p=30d&f=d,c,h,l,o,v')
+        f = open('./data/temp', 'w')
+        f.write(data)
+        f.close()
+
+def parsefile(lines):
+
+#get a dict containing #days, day name, #mins and min data for temp file (use to compare to saved file to append new data)      
+	tempfile = {}
+	days = []
+	mins = 0
+	for x in range(len(lines)):
+		split = lines[x].split(',')
+		if str(split[0]).startswith('a'):
+			currentday = split[0]
+			days.append(currentday)
+			tempfile[currentday + 'days'] = split[0]
+			tempfile[currentday + 'mins'] = mins
+			tempfile[currentday + 'data'] = ''
+			tempfile[currentday + 'data'] += str(lines[x])
+		else:
+			if len(days) != 0 and x + 1 != len(lines):
+				tempfile[str(currentday) + 'mins'] += 1
+				tempfile[str(currentday) + 'data'] += str(lines[x])
+
+	return tempfile, days
+
+
+def minmax(dictionary, dataset):
+
+	lowval = dictionary[str(0) + dataset]
+	lowmin = 0
+	lowamount = 0
+	highval = dictionary[str(0) + dataset]
+	highmin = 0
+	highamount = 0
+
+	for x in range(390):
+		if dictionary[str(x) + dataset] < lowval:
+			lowval = dictionary[str(x) + dataset]
+			lowmin = x
+			lowamount = dictionary[str(x) + 'close']
+		if dictionary[str(x) + dataset] > highval:
+			highval = dictionary[str(x) + dataset]
+			highmin = x
+			highamount = dictionary[str(x) + 'close']
+
+	# calculate times (based on market open (6:30 GMT+8)
+	today = datetime.datetime.today()
+	marketopen = datetime.datetime(today.year, today.month, today.day, 6, 30)
+	lowdelta = datetime.timedelta(minutes = int(lowmin))
+	highdelta = datetime.timedelta(minutes = int(highmin))
+	marketlow = marketopen + lowdelta
+	markethigh = marketopen + highdelta
+
+
+	return lowval, marketlow, lowamount, highval, markethigh, highamount
+
+	
+
+
+
+
+
+
--- percentchange.py
+++ percentchange.py
@@ -1,99 +1,107 @@
 #!/usr/bin/env python3
 
 import sys
-import requests
 import os
 import datetime
-from bs4 import BeautifulSoup, NavigableString
+from intradaytools import *
 
-def scrape(website):
-	set_url = website
-	cal_resp = requests.get(set_url)
-	cal_data = cal_resp.text
-	data = BeautifulSoup(cal_data, 'lxml')
-
-	return str(data)
 
 def main():
+
 	os.system('clear')
 
 	# Get ticker from user
 	ticker = input('Enter Ticker: ')
 	market = input('Choose Market (NYSE || NASD): ')
 
-	# scrape data and write to a file
-	data = scrape('https://www.google.com/finance/getprices?q=' + str(ticker.upper()) + '&x=' + str(market.upper()) + '&i=60&p=30d&f=d,c,h,l,o,v')
-	f = open('./data/' + ticker, 'w')
-	f.write(data)
-	f.close()
+	# get intraday information for the last 15 days, save to temp file
+	temp(ticker, market)
 
-	# remove lines 1-7 and the last line, then override file
-	lines = open('./data/' + ticker).readlines()
-	newfile = open('./data/' + ticker,'w').writelines(lines[7:-1])
+	# open tempfile, save lines, delete temp file.
+	templines = open('./data/temp').readlines()
+	os.remove('./data/temp')
 
-	# open new file and use data
+	# check for exsisting data files
+	filecheck = os.path.isfile('./data/' + ticker)
+
+	# create new ticker data file if one does not exist or add new lines lines to existing files
+	if filecheck == False:
+		newfile = open('./data/' + ticker,'w').writelines(templines[7:-1])
+	else:
+		# parse exsiting data files, and new temp data files
+		tempfile, tempdays = parsefile(templines)
+		existinglines = open('./data/' + ticker).readlines()
+		existingfile, existingdays = parsefile(existinglines)	
+
+		# merge files, get data older than 15 days from existing file & get data within current 15 days from temp file
+		mergedfile = ''
+		for x in range(len(existingdays)):
+			if existingdays[x] in tempdays:
+				# recent data within the last 15 days - should append from new data (tempfile)
+				pass
+			else:
+				# older data no longer in the last 15 days - append from existing data (existingfile)
+				mergedfile += str(tempfile[str(tempdays[x]) + 'data'])
+
+		# append all new data (will override old similar data, and keep current day up todate)
+		for x in range(len(tempdays)):
+			mergedfile += str(tempfile[str(tempdays[x]) + 'data'])
+
+		# overide exsiting file with new data
+		override = open('./data/' + ticker, 'w')
+		override.write(mergedfile)
+		override.close()
+
+	# open ticker data file and use data
 	data = open('./data/' + ticker).readlines()
 
-	# create dictionary for data storage && store data / calculate pct change per min per day
+	# create dictionary for data storage && store data / run calculations for each min (390 mins per day) 
 	intraday = {}
 	average = {}
 	day = 0
-	for line in data:
+	for line in data: # every line = 1 min of data
 		split = line.split(',')
-		if str(split[0]).startswith('a'): # replace unix time stamp for new day with 0, to represent minute 0 rather than new day
-			split[0] = str(0)
+		if str(split[0]).startswith('a'): # replace unix time stamp (which starts with 'a') for new day with 0
+			split[0] = str(0) # split[0] = int ranging 0 - 390. represents time: 0 = 6:30 (market open) 390 = 13:00 (market close) 
 
-		intraday[str(day) + split[0] + 'high'] = float(split[2]) # split[0] = min, [1] = close, [2] = high, [3] = low, [4] = open, [5] = volume
-		intraday[str(day) + split[0] + 'low'] = float(split[3])
-		intraday[str(day) + split[0] + 'open'] = float(split[4])
-		intraday[str(day) + split[0] + 'close'] = float(split[1])
-		intraday[str(day) + split[0] + 'vol'] = int(split[5])
-		intraday[str(day) + split[0] + 'pctchange'] = ((float(split[1]) / float(split[4])) - int(1)) * int(100)
+		intraday[str(day) + split[0] + 'high'] = float(split[2]) # split[2] = current min high $ value 
+		intraday[str(day) + split[0] + 'low'] = float(split[3]) # split[3] = current min low $ value
+		intraday[str(day) + split[0] + 'open'] = float(split[4]) # split[4] = current min open $ value
+		intraday[str(day) + split[0] + 'close'] = float(split[1]) # split[1] = current min close $ value
+		intraday[str(day) + split[0] + 'vol'] = int(split[5]) # split[5] = current min volume # of shares
 
-		average[split[0] + 'avgpctchange'] = 0
+		# formulas
+		intraday[str(day) + split[0] + 'volatilityByMin'] = ((float(split[1]) / float(split[4])) - int(1)) * int(100)
+		intraday[str(day) + split[0] + 'volatilityVsMktOpen'] = ((float(split[1]) / float(intraday[(str(day)) + '0' + 'open'])) - int(1)) * int(100)
+
+		# create storage locations for averages
+		average[split[0] + 'volatilityByMin'] = 0
+		average[split[0] + 'volatilityVsMktOpen'] = 0
 		average[split[0] + 'close'] = 0
 
+		# increment day counter any time current min == 390 (market close / end of current day)
 		if int(split[0]) == int(390):
 			day += 1
 
-	# add pctchanges for each min
+	# summation for all individual days data 
 	for x in range(day):
 		for x2 in range(390):
-			average[str(x2) + 'avgpctchange'] += float(intraday[str(x) + str(x2) + 'pctchange'])	
+			average[str(x2) + 'volatilityByMin'] += float(intraday[str(x) + str(x2) + 'volatilityByMin'])	
+			average[str(x2) + 'volatilityVsMktOpen'] += float(intraday[str(x) + str(x2) + 'volatilityVsMktOpen'])
 			average[str(x2) + 'close'] += float(intraday[str(x) + str(x2) + 'close'])
 
-	# calculate average pctchange
+	# calculate averages based on above summation / # of days of data stored in ticker file
 	for x in range(390):
-		average[str(x) + 'avgpctchange'] /= int(day)
+		average[str(x) + 'volatilityByMin'] /= int(day)
+		average[str(x) + 'volatilityVsMktOpen'] /= int(day)
 		average[str(x) + 'close'] /= int(day)
 
 	# find min and max
-	lowval = average[str(0) + 'avgpctchange']
-	lowmin = 0
-	lowamount = 0
-	highval = average[str(0) + 'avgpctchange']
-	highmin = 0
-	highamount = 0
-	for x in range(390):
-		if average[str(x) + 'avgpctchange'] < lowval:
-			lowval = average[str(x) + 'avgpctchange']
-			lowmin = x
-			lowamount = average[str(x) + 'close']
-		if average[str(x) + 'avgpctchange'] > highval:
-			highval = average[str(x) + 'avgpctchange']
-			highmin = x
-			highamount = average[str(x) + 'close']
+	lowvalVBM, lowtimeVBM, lowamountVBM, highvalVBM, hightimeVBM, highamountVBM = minmax(average, 'volatilityByMin')
 
-	# calculate time
-	today = datetime.datetime.today()
-	marketopen = datetime.datetime(today.year, today.month, today.day, 6, 30)
-	lowdelta = datetime.timedelta(minutes = int(lowmin))
-	highdelta = datetime.timedelta(minutes = int(highmin))
-	marketlow = marketopen + lowdelta
-	markethigh = marketopen + highdelta
 
 	
-	print('%s: Average +/- percent change per min (Last 15 days) - Highest: $%.2f @ +%.4f%% (%s) | Lowest: $%.2f @ %.4f%% (%s) ' % (str(ticker.upper()), highamount, highval, str(markethigh.strftime("%I:%M%p")), lowamount, lowval, str(marketlow.strftime("%I:%M%p"))))
+	print('%s: Volatility by min (Last %s days) - Highest: $%.2f @ +%.4f%% (%s) | Lowest: $%.2f @ %.4f%% (%s) ' % (str(ticker.upper()), day, highamountVBM, highvalVBM, str(hightimeVBM.strftime("%I:%M%p")), lowamountVBM, lowvalVBM, str(lowtimeVBM.strftime("%I:%M%p"))))
 
 
 
