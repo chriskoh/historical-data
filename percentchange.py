09a0833aa8d128265da991a29ba9250161c761a6
chriskoh <chris.koh90@gmail.com>
2016-06-30 08:59:19 -0700

    added market input, reworked avg algorithm, changed output string

--- percentchange.py
+++ percentchange.py
@@ -18,10 +18,11 @@ def main():
 	os.system('clear')
 
 	# Get ticker from user
-	var = input('Enter Ticker: ')
+	ticker = input('Enter Ticker: ')
+	market = input('Choose Market (NYSE || NASD): ')
 
 	# scrape data and write to a file
-	data = scrape('https://www.google.com/finance/getprices?q=' + str(var.upper()) + '&x=NASD&i=60&p=30d&f=d,c,h,l,o,v')
+	data = scrape('https://www.google.com/finance/getprices?q=' + str(ticker.upper()) + '&x=' + str(market.upper()) + '&i=60&p=30d&f=d,c,h,l,o,v')
 	f = open('datatest', 'w')
 	f.write(data)
 	f.close()
@@ -33,67 +34,66 @@ def main():
 	# open new file and use data
 	data = open('datatest').readlines()
 
-	# create dictionary for data storage
+	# create dictionary for data storage && store data / calculate pct change per min per day
 	intraday = {}
-	count = 0
+	average = {}
+	day = 0
 	for line in data:
 		split = line.split(',')
 		if str(split[0]).startswith('a'): # replace unix time stamp for new day with 0, to represent minute 0 rather than new day
 			split[0] = str(0)
-		intraday[split[0] + 'high'] = 0 
-		intraday[split[0] + 'low'] = 0
-		intraday[split[0] + 'open'] = 0
-		intraday[split[0] + 'close'] = 0
 
+		intraday[str(day) + split[0] + 'high'] = float(split[2]) # split[0] = min, [1] = close, [2] = high, [3] = low, [4] = open, [5] = volume
+		intraday[str(day) + split[0] + 'low'] = float(split[3])
+		intraday[str(day) + split[0] + 'open'] = float(split[4])
+		intraday[str(day) + split[0] + 'close'] = float(split[1])
+		intraday[str(day) + split[0] + 'vol'] = int(split[5])
+		intraday[str(day) + split[0] + 'pctchange'] = ((float(split[1]) / float(split[4])) - int(1)) * int(100)
 
-	# populate dictionary
-	for line in data:
-		split = line.split(',')
-		if str(split[0]).startswith('a'):
-			split[0] = str(0)
-		intraday[split[0] + 'high'] += float(split[2]) 
-		intraday[split[0] + 'low'] += float(split[3])
-		intraday[split[0] + 'open'] += float(split[4])
-		intraday[split[0] + 'close'] += float(split[1])
+		average[split[0] + 'avgpctchange'] = 0
+		average[split[0] + 'close'] = 0
 
-	# divide data by 15 (max number of days pulled by google finance intraday)
+		if int(split[0]) == int(390):
+			day += 1
+
+	# add pctchanges for each min
+	for x in range(day):
+		for x2 in range(390):
+			average[str(x2) + 'avgpctchange'] += float(intraday[str(x) + str(x2) + 'pctchange'])	
+			average[str(x2) + 'close'] += float(intraday[str(x) + str(x2) + 'close'])
+
+	# calculate average pctchange
 	for x in range(390):
-		intraday[str(x) + 'high'] /= int(15)
-		intraday[str(x) + 'low'] /= int(15)
-		intraday[str(x) + 'open'] /= int(15)
-		intraday[str(x) + 'close'] /= int(15)
+		average[str(x) + 'avgpctchange'] /= int(day)
+		average[str(x) + 'close'] /= int(day)
 
-	# calculation variables
-	today = datetime.datetime.today()
-	marketopen = datetime.datetime(today.year, today.month, today.day, 6, 30)
-	low = 1000
-	lowest = 0
-	high = 0
-	highest = 0
-
-	# calculate data based on close price for each min
+	# find min and max
+	lowval = average[str(0) + 'avgpctchange']
+	lowmin = 0
+	lowamount = 0
+	highval = average[str(0) + 'avgpctchange']
+	highmin = 0
+	highamount = 0
 	for x in range(390):
-		if intraday[str(x) + 'close'] < low: # average lowest close min in the last 15 days
-			low = intraday[str(x) + 'close']
-			lowest = x
-		if intraday[str(x) + 'close'] > high: # average highest close min in the last 15 days
-			high = intraday[str(x) + 'close']
-			highest = x
+		if average[str(x) + 'avgpctchange'] < lowval:
+			lowval = average[str(x) + 'avgpctchange']
+			lowmin = x
+			lowamount = average[str(x) + 'close']
+		if average[str(x) + 'avgpctchange'] > highval:
+			highval = average[str(x) + 'avgpctchange']
+			highmin = x
+			highamount = average[str(x) + 'close']
 
 	# calculate time
-	lowdelta = datetime.timedelta(minutes = int(lowest))
-	highdelta = datetime.timedelta(minutes = int(highest))
+	today = datetime.datetime.today()
+	marketopen = datetime.datetime(today.year, today.month, today.day, 6, 30)
+	lowdelta = datetime.timedelta(minutes = int(lowmin))
+	highdelta = datetime.timedelta(minutes = int(highmin))
 	marketlow = marketopen + lowdelta
 	markethigh = marketopen + highdelta
 
 	
-	print(str(var.upper()) + ' last 15 days (Average intraday) - Highest: $' + str(high) + '(' + str(markethigh) + ')' + ' | Lowest: $' + str(low) + '(' + str(marketlow) + ')')
-
-
-
-
-
-
+	print('%s: Average +/- percent change per min (Last 15 days) - Highest: $%.2f @ +%.4f%% (%s) | Lowest: $%.2f @ %.4f%% (%s) ' % (str(ticker.upper()), highamount, highval, str(markethigh.strftime("%I:%M%p")), lowamount, lowval, str(marketlow.strftime("%I:%M%p"))))
 
 
 
